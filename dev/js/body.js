d9aac0459d31ff46db0cf9802fbcaaf034891c63
Jérôme Skoda <contact@jeromeskoda.fr>
2018-08-15 13:15:24 +0200

    Remove gmap, add izimodal

--- dev/js/body.js
+++ dev/js/body.js
@@ -4,6 +4,4 @@
 //= vendor/jquery.js
 //= lib/jquery.navigation.js
 //= lib/settings.js
-
-// Google map disabled by default because of empty API key
-// It is still available at lib/gmap.js
+//= lib/iziModal.js
